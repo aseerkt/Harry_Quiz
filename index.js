343acf5d66f9bac44b7918a9682e01698bad04ee
Aseer KT <aseermiqdad@gmail.com>
2021-05-30 14:26:38 +0530

    fixed wrong option bug

--- index.js
+++ index.js
@@ -1,93 +1,16 @@
 //Harry Quiz
 require('colors');
-let readLineSync = require('readline-sync');
+const readLineSync = require('readline-sync');
+const questions = require('./questions');
 
 let score = 0;
 
 function welcome() {
   let name = readLineSync.question('What is your name?\n');
   console.log(
-    `Welcome ${name}! Let us see how well do you know Harry Potter?\n`
+    `Welcome ${name}! Let us see how well do you know Harry Potter\n`
   );
 }
-
-let questions = [
-  {
-    question: "Name of Harry Potter's Father",
-    options: ['Severus Snape', 'Gandalf', 'Voldemort', 'James Potter'],
-    answer: 4,
-  },
-  {
-    question: "Name of Harry Potter's owl",
-    options: ['Hedwig', 'Fluffy', 'Padfoot', 'Norbert'],
-    answer: 1,
-  },
-  {
-    question: "Name Draco Malfoy's Dad.",
-    options: [
-      'Narcissa Malfoy',
-      'John Malfoy',
-      'Lucius Malfoy',
-      'James Malfoy',
-    ],
-    answer: 3,
-  },
-  {
-    question: "Name of Lord Voldemort's loyal snake",
-    options: ['Padfoot', 'Norbert', 'Fang', 'Nagini'],
-    answer: 4,
-  },
-  {
-    question: 'The `Killing Curse` spell of Voldemort',
-    options: [
-      'Expecto patronum',
-      'Avada Kedavra',
-      'Wingardium Leviosa',
-      'Expelliarmus',
-    ],
-    answer: 2,
-  },
-  {
-    question: 'What is the spell used for disarming people?',
-    options: [
-      'Expecto patronum',
-      'Avada Kedavra',
-      'Wingardium Leviosa',
-      'Expelliarmus',
-    ],
-    answer: 4,
-  },
-  {
-    question: "Who is Harry Potter's godfather?",
-    options: ['Voldemort', 'Hagrid', 'Tommy Angelo', 'Sirius Black'],
-    answer: 4,
-  },
-  {
-    question: "What was Ron's pet name?",
-    options: ['Scrabbers', 'Norbert', 'Fang', 'Hedwig'],
-    answer: 1,
-  },
-  {
-    question: 'Who did Draco Malfoy called Mudblood?',
-    options: ['Hermione', 'Ron Weasley', 'Harry Potter', 'Ginny'],
-    answer: 1,
-  },
-  {
-    question: 'Who married Harry Potter?',
-    options: ['Hermione', 'Luna Lovegood', 'Ginny Weasely', 'Cho Chang'],
-    answer: 3,
-  },
-  {
-    question: 'Where did exactly Harry meet Tom Riddle for the first time?',
-    options: [
-      'Hogwarts',
-      'Chamber of Secrets',
-      'Little Hangleton',
-      "Godric's Hollow",
-    ],
-    answer: 2,
-  },
-];
 
 function askQuestion(question) {
   // Format options to display
@@ -106,18 +29,18 @@ function askQuestion(question) {
   if (![1, 2, 3, 4].includes(userOption)) {
     console.log(`You have given wrong options, Please try again`);
     askQuestion(question);
-  }
-
-  // Calculate score
-  if (userOption === question.answer) {
-    console.log('\nRight'.green);
-    score++;
   } else {
-    console.log('\nWrong'.red);
-  }
+    // Calculate score
+    if (userOption === question.answer) {
+      console.log('\nRight'.green);
+      score++;
+    } else {
+      console.log('\nWrong'.red);
+    }
 
-  console.log(`Current Score: ${score}`.yellow.bold);
-  console.log('-------------');
+    console.log(`Current Score: ${score}`.yellow.bold);
+    console.log('-------------');
+  }
 }
 
 function game() {
---
+++ questions.js
@@ -0,0 +1,77 @@
+module.exports = [
+  {
+    question: "Name of Harry Potter's Father",
+    options: ['Severus Snape', 'Gandalf', 'Voldemort', 'James Potter'],
+    answer: 4,
+  },
+  {
+    question: "Name of Harry Potter's owl",
+    options: ['Hedwig', 'Fluffy', 'Padfoot', 'Norbert'],
+    answer: 1,
+  },
+  {
+    question: "Name Draco Malfoy's Dad.",
+    options: [
+      'Narcissa Malfoy',
+      'John Malfoy',
+      'Lucius Malfoy',
+      'James Malfoy',
+    ],
+    answer: 3,
+  },
+  {
+    question: "Name of Lord Voldemort's loyal snake",
+    options: ['Padfoot', 'Norbert', 'Fang', 'Nagini'],
+    answer: 4,
+  },
+  {
+    question: 'The `Killing Curse` spell of Voldemort',
+    options: [
+      'Expecto patronum',
+      'Avada Kedavra',
+      'Wingardium Leviosa',
+      'Expelliarmus',
+    ],
+    answer: 2,
+  },
+  {
+    question: 'What is the spell used for disarming people?',
+    options: [
+      'Expecto patronum',
+      'Avada Kedavra',
+      'Wingardium Leviosa',
+      'Expelliarmus',
+    ],
+    answer: 4,
+  },
+  {
+    question: "Who is Harry Potter's godfather?",
+    options: ['Voldemort', 'Hagrid', 'Tommy Angelo', 'Sirius Black'],
+    answer: 4,
+  },
+  {
+    question: "What was Ron's pet name?",
+    options: ['Scrabbers', 'Norbert', 'Fang', 'Hedwig'],
+    answer: 1,
+  },
+  {
+    question: 'Who did Draco Malfoy called Mudblood?',
+    options: ['Hermione', 'Ron Weasley', 'Harry Potter', 'Ginny'],
+    answer: 1,
+  },
+  {
+    question: 'Who married Harry Potter?',
+    options: ['Hermione', 'Luna Lovegood', 'Ginny Weasely', 'Cho Chang'],
+    answer: 3,
+  },
+  {
+    question: 'Where did exactly Harry meet Tom Riddle for the first time?',
+    options: [
+      'Hogwarts',
+      'Chamber of Secrets',
+      'Little Hangleton',
+      "Godric's Hollow",
+    ],
+    answer: 2,
+  },
+];
